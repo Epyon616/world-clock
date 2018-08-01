a666f50c126692df0acc78f1f897d9747aaf5ea1
Lee Richmond <lrichmond@signstix.com>
2018-08-01 09:29:58 +0100

    initialize project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Clock from 'react-live-clock';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Clock format={'HH:mm:ss'} ticking={true} timezone={'Europe/London'} />
+      </div>
+    );
+  }
+}
+
+export default App;
