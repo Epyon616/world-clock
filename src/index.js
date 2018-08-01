01aed897cc4cf6755dd6a13c225a5eabfc97f7e1
Lee Richmond <lrichmond@signstix.com>
2018-08-01 14:19:52 +0100

    Update references to main clock component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './stylesheets/index.css';
 import '../stylesheets/App.css';
+import WorldClock from './components/WorldClock';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+  <WorldClock />,
