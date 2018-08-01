d9c1199f3354ead016e5c77411ab9c600dd1072f
Lee Richmond <lrichmond@signstix.com>
2018-08-01 15:13:22 +0100

    fix typo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
-import '../stylesheets/App.css';
+import './stylesheets/App.css';
 import WorldClock from './components/WorldClock';
 
 ReactDOM.render(
